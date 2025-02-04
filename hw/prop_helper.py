22aae573af4fb3164f26227a6a8ec6a9ed222b4a
bigE4 <ianjellsworth@gmail.com>
2025-02-04 16:51:42 -0700

    prop_helper.py written!

---
+++ hw/prop_helper.py
@@ -0,0 +1,21 @@
+x1 = .4896
+x2 = .4931
+x3 = .4885
+x4 = .3964
+x5 = .3667
+y1 = -.0187
+y2 = -.1048
+z1 = -1
+z2 = .4
+z3 = 1
+z4 = .5489
+
+print(x1 + (y1 * z1))
+print(x1 + (y1 * z2))
+print(x2 + (y1 * z1))
+print(x2 + (y1 * z2))
+print(x3 + (y1 * z3))
+print(x3 + (y1 * z3))
+print(x4 + (y2 * z4))
+print(x4 + (y2 * z4))
+print(x4 + (y2 * z3))
